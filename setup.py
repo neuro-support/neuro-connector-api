77e7eba6f284e8d3cb2e84c667f2ceae76c7b788
Ben Hesketh <ben.hesketh@vive.co.uk>
2023-03-09 19:02:13 +0100

    setup.py and cl support

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
    name='neuro-connector-api',
    version='v1.0.1-alpha',
    description='Connects to app.myneuro.ai',
+   long_description='Intended to be used in the command line. NeuroConnector.py -c [connectonId] -o [organizationId] -u [baseUrl] -a [appToken] -f [function] -p [filePath]\nFunctions [1=sendTestResultsJson]\n',
    author='Ben Hesketh',
    author_email='bhesketh@wearedragonfly.co',
    packages=['neuro-connector-api'],  #same as name
